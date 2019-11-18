7a957892fb159104df9565af56ce3a8a7ad29d96
Alfredo_CU7557 <alfredo_11_cu@hotmail.com>
2019-11-18 11:08:29 -0600

    Se crearon pruebas para entener las diferentes formas de crear componentes, con funciones (normales o arrow) o clases

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+/*
+function HelloWord(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 id="Hello"> 
+        Hola Mundo {props.name} 
+      </h1>
+      <h2>
+        {props.subtitle}
+      </h2>
     </div>
   );
 }
+*/
+
+class HelloWord extends React.Component {
+  state = {
+    show: true
+  }
+
+  cambiarEstado = () => {
+    this.setState({ show: !this.state.show});
+  }
+
+  render(){
+    if(this.state.show) {
+      return(
+        <div>
+          <h1 id="Hello">
+            Hola Mundo {this.props.name}
+          </h1>
+          <h2>
+            {this.props.subtitle}
+          </h2>
+          <button onClick={this.cambiarEstado}> Cambiar estado </button>
+        </div>
+      );
+    }
+    else {
+      return(
+        <div>
+        <h1> No hay elementos </h1>
+        <button onClick={this.cambiarEstado}> Cambiar estado </button>
+        </div>
+      );
+    }
+  }
+}
+
+/* Componentes con funciones Arrow.
+const App = () => (
+    <div> 
+      <HelloWord/> <HelloWord/>
+    </div>
+);
+*/
+
+// Componentes con funciones normales.
+function App() {
+  return (
+    <div className="App">
+      <HelloWord name="Alfredo" subtitle="Componente 1"/> 
+      <HelloWord name="Angelica" subtitle="Componente 2"/>
+    </div>
+  );
+}
+
+/* Componentes con clases.
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <HelloWord/> <HelloWord/>
+      </div>
+    );
+  }
+}
+*/
 
 export default App;
